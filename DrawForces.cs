0fb8428c5c93aa5a773593fde1166522f3b8b935
andrei <andrei.fzto@gmail.com>
2016-03-30 18:48:17 +0300

    start physics

---
+++ DrawForces.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+[RequireComponent (typeof(PhysicsEngine))]
+
+public class DrawForces : MonoBehaviour 
+{
+
+    public bool showForces;
+
+    private List<Vector3> forceVectorList;
+
+    void OnDrawGizmos()
+    {
+        if (!showForces)
+            return;
+        
+        forceVectorList = GetComponent<PhysicsEngine>().forceVectorList;
+
+        Gizmos.color = Color.cyan;
+        forceVectorList.ForEach(RayTo);
+
+        Gizmos.color = Color.red;
+        RayTo(forceVectorList.Aggregate(Vector3.zero, (acc, vector) => acc + vector));
+    }
+        
+
+    void RayTo (Vector3 vec)
+    {
+        Gizmos.DrawRay(transform.position, vec);
+    }
+
+}
---
+++ PhysicsEngine.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PhysicsEngine : MonoBehaviour 
+{
+
+    public Vector3 velocityVector;
+    public Vector3 netForceVector;
+    public float mass;
+    public List<Vector3> forceVectorList = new List<Vector3>();
+	
+	void FixedUpdate () 
+   	{  
+        transform.position += forceVectorList.Aggregate(velocityVector + netForceVector, (acc, v) => acc + v) / mass * Time.deltaTime;
+	}
+
+    void UpdateVelocity()
+    {
+
+    }
+
+}
